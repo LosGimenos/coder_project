43cfe677f805c629d6d4a98b141601e38b6d53d0
Larry Hernandez <larryhernanjr@gmail.com>
2017-11-10 18:20:36 -0500

    layer logic to project select

--- coder_view/urls.py
+++ coder_view/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'coder_view'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<coder_id>[0-9]+)/choose_project/$', views.select_project, name='coder_splash')
 ]
--- coder_view/views.py
+++ coder_view/views.py
@@ -2,10 +2,31 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect, HttpResponse
-from coder_app.models import Project, Tag, Variable
+from coder_app.models import Project, Tag, Variable, Coder, Row
 
 def index(request):
+    coder_data = Coder.objects.all()
 
-    return render(request, 'coder_view/index.html', context={})
+    return render(request, 'coder_view/index.html', {'coder_data': coder_data})
+
+def select_project(request, coder_id):
+    coder = Coder.objects.get(id=coder_id)
+    projects = coder.project_set.all()
+    project_data = []
+
+    for project in projects:
+        rows = Row.objects.filter(project=project)
+        row_count = rows.count()
+
+        single_project = {
+            'name': project.name,
+            'rate': project.rate,
+            'row_count': row_count
+        }
+
+        project_data.append(single_project)
+
+    return render(request, 'coder_view/select_project.html', context={'coder_data': coder, 'project_data': project_data})
+
 
 
